ebf0c429fef8164bbb70ee7e8f1b6ac3d4806156
Gregory Sobotka <gregorysobotka@gmail.com>
2016-02-21 15:27:55 -0800

    background image interior pages
    Added custom message to background image if bundle id is not present.
    Added background to create page.

--- urlBundleApp/public/templates/compiled/create.js
+++ urlBundleApp/public/templates/compiled/create.js
@@ -1,6 +1,6 @@
 define(['hb'], function (Handlebars) {
 var template = Handlebars.template, templates = App.hb.templates = App.hb.templates || {};
 templates['create'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "empty\r\n\r\n";
+    return "empty\r\n todo : move create form to this hb template\r\n";
 },"useData":true});return templates['create'];
 });
