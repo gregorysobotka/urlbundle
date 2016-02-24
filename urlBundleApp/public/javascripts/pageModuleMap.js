16b869c9307122786f719ed4c5cf7d34505f96df
Gregory Sobotka <gregorysobotka@gmail.com>
2016-02-24 00:11:17 -0800

    Home page content
    Moving images into same div's as content above them

--- urlBundleApp/public/javascripts/pageModuleMap.js
+++ urlBundleApp/public/javascripts/pageModuleMap.js
@@ -1,10 +1,27 @@
 define(function() {
 
+    /*
+
+        This config contains auto-loaded modules and submodules
+
+    */
+
     return {
-        home :  { modules : ['modules/homePage'], subModules : ['subModules/randomBackground','subModules/homePageResponsive'] },
-        create : { modules : ['modules/create'], subModules : ['subModules/randomGenerator'] },
-        editBundle : { modules : ['modules/edit'], subModules : ['subModules/editReorder'] },
-        viewBundle : { modules : ['modules/viewBundle'] }
+        home :  {
+            modules : ['modules/homePage'],
+            subModules : ['subModules/randomBackground','subModules/homePageResponsive']
+        },
+        create : {
+            modules : ['modules/create'],
+            subModules : ['subModules/randomGenerator']
+        },
+        editBundle : {
+            modules : ['modules/edit'],
+            subModules : ['subModules/editReorder']
+        },
+        viewBundle : {
+            modules : ['modules/viewBundle']
+        }
     };
 
 });
