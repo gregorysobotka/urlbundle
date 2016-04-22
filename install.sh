b8ea3e96524a3b81033f9b6a1d2c3204395b64dc
Gregory Sobotka <gregorysobotka@gmail.com>
2016-04-22 15:01:34 -0700

    AWS deploy
    Testing changes for AWS EC2 deploy (vs digital ocean nodejs instance)

--- install.sh
+++ install.sh
@@ -3,14 +3,23 @@
 sudo apt-get update
 sudo apt-get install git
 
-#global installs
-sudo npm install pm2 -g
+git clone git://github.com/nodejs/node.git
+cd node
+./configure
+make
+sudo make install
+
+cd ../
+cd urlbundle
 
 git clone git@github.com:gregorysobotka/urlbundle.git
 
 #install app
 cd urlBundleApp
 sudo npm install
+
+#global installs
+sudo npm install pm2 -g
 
 #start app
 pm2 start bin/www
